36923e8e9daca1dbaa25294759b4db83048c2cb8
shimin <mandyshimin18@gmail.com>
2023-06-23 22:02:37 -0400

    JUN 23

--- src/main/java/com/imooc/mall/controller/UserController.java
+++ src/main/java/com/imooc/mall/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.imooc.mall.controller;
 
-import com.imooc.mall.pojo.User;
+import com.imooc.mall.enums.ResponseEnum;
+import com.imooc.mall.form.UserForm;
 import com.imooc.mall.vo.ResponseVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/user")
@@ -19,8 +24,16 @@ public class UserController {
 //        log.info("username={}",username);
 //    }
     //json
-    public ResponseVo register(@RequestBody User user){
-        log.info("username={}",user.getUsername());
-        return ResponseVo.success("注册成功");
+    public ResponseVo register(@Valid @RequestBody UserForm userForm, BindingResult bindingResult){
+        //表单验证
+        if (bindingResult.hasErrors()) {
+            log.error("注册提交的参数有误，{} {}",
+                    bindingResult.getFieldError().getField(),
+                    bindingResult.getFieldError().getDefaultMessage());
+            return ResponseVo.error(ResponseEnum.PARAM_ERROR, bindingResult);
+        }
+        log.info("username={}",userForm.getUsername());
+        //return ResponseVo.success("注册成功");
+        return ResponseVo.error(ResponseEnum.NEED_LOGIN);
     }
 }
