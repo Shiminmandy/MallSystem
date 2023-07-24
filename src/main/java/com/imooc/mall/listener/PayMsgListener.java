69ec9353b0ee6f0c17f035a7dd136a3bc0e4ffff
shimin <mandyshimin18@gmail.com>
2023-07-24 22:37:00 +0800

    Jul 24

---
+++ src/main/java/com/imooc/mall/listener/PayMsgListener.java
@@ -0,0 +1,16 @@
+package com.imooc.mall.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "payNotify")
+@Slf4j
+public class PayMsgListener {
+    @RabbitHandler
+    public void process(String msg){
+        log.info("【接收到消息】=> {}",msg);
+    }
+}
